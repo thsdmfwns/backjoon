2684e3c157dbefe8f4035fcfff8c9e0cc1e18897
Son-Desktop <son2044@gmail.com>
2025-04-29 18:24:00 +0900

    BOJ-EX: 2025. 4. 29. 오후 6:23:57

--- 2869번 - 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cpp
+++ 2869번 - 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cpp
@@ -9,3 +9,13 @@
 /*   Solved: 2025/04/29 18:15:36 by thsdmfwns     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int a, b, v;
+    cin >> a >> b >> v;
+    cout << int(ceil((double(v - a) / (a - b)))) +1 ;
+}
