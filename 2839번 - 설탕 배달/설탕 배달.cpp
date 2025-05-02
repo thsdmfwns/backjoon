c18c69079630dd41f626db1a27e6f5c644e44807
Son-Desktop <son2044@gmail.com>
2025-05-02 15:03:06 +0900

    BOJ-EX: 2025. 5. 2. 오후 3:03:03

--- 2839번 - 설탕 배달/설탕 배달.cpp
+++ 2839번 - 설탕 배달/설탕 배달.cpp
@@ -9,3 +9,34 @@
 /*   Solved: 2025/05/02 14:56:30 by thsdmfwns     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    if(n % 5 == 0){
+        cout << n/5;
+        return 0; 
+    }
+
+    int num = n;
+    int count = 0;
+    while(num > 0){
+        num -= 3;
+        count++;
+        if(num % 5 == 0){
+            count += num/5;
+            num = 0;
+        }
+    }
+    if(num < 0){
+        cout << -1;
+    }
+    else{
+        cout << count;
+    }
+}
